74a9574e7ed0d3a7d1edc6718c61735d00f37979
Juncheoltree <soultreejun@gmail.com>
2025-05-27 10:41:50 +0900

    Initial Commit

---
+++ WAY_GYM/WAY_GYMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAY_GYMApp.swift
+//  WAY_GYM
+//
+//  Created by Leo on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WAY_GYMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
