3cd07a3aa40bd70d5715774d185a4eb12c648f48
Juncheoltree <soultreejun@gmail.com>
2025-05-28 12:27:42 +0900

    initial commit

---
+++ WAY_GYM/View/GangView.swift
@@ -0,0 +1,18 @@
+//
+//  GangView.swift
+//  WAY_GYM
+//
+//  Created by Leo on 5/28/25.
+//
+
+import SwiftUI
+
+struct GangView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GangView()
+}
