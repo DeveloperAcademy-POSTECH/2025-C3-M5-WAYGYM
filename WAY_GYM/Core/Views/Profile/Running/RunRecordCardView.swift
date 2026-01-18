9e5e92d7187fd62819f5aacf94ff38d334eb6e5a
JUDY LEE <ljh.social@ewhain.net>
2026-01-18 14:39:31 +0900

    feat: 내 구역 조건 수정 및 기록 이미지 그리도록

---
+++ WAY_GYM/Core/Views/Profile/Running/RunRecordCardView.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  WAY_GYM
+//
+//  Created by 이주현 on 1/18/26.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
